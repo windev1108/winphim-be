198d10631a200c4b195fdadfa5384213666e30ff
Win1 <vuhuynh@var-meta.com>
2026-01-17 22:51:50 +0700

    fix: debug be

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -11,7 +11,8 @@ export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest<Request>();
         const session = (request as any).session;
-        if (!session || !session.user?.id) {
+        console.log({ session })
+        if (!session || !session?.user?.id) {
             throw new UnauthorizedException('Please login to access this resource');
         }
 
